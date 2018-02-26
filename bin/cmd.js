e44ab15ffa458af66729c8ca6b73513a425d2b92
Stephen Whitmore <sww@eight45.net>
2018-02-26 14:43:46 -0800

    dev: fixes; prompt user before downloading

--- bin/cmd.js
+++ bin/cmd.js
@@ -4,23 +4,48 @@ var minimistAliases = { 'z': 'zoom', 't': 'template', 'r': 'radius' }
 var argv = require('minimist')(process.argv, { alias: minimistAliases })
 var fs = require('fs')
 var path = require('path')
+var readline = require('readline')
 
 if (!argv.zoom || !argv.lat || !argv.lon || !argv.radius || !argv.template ||
   !isTemplateValid(argv.template)) {
   printUsageAndExit()
 }
 
-var bbox = toRect(Number(argv.lat), Number(argv.lon), Number(argv.radius))
+var bbox = toRect(Number(argv.lat), Number(argv.lon), Number(argv.radius), Number(argv.zoom))
 var tiles = tilesInRect(bbox)
 
 var mapFn = tileToUrlMapper(argv.template, Number(argv.zoom))
 var urls = tiles.map(mapFn)
 
+var text = 'This will make ' + urls.length + ' HTTP requests. Proceed? [y/N] '
+prompt(text, function (err, proceed) {
+  if (err) throw err
+  if (proceed) {
+    console.log('TODO: download')
+  } else {
+    console.log('aborted')
+    process.exit(1)
+  }
+})
+
+function prompt (text, cb) {
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+
+  rl.question(text, function (answer) {
+    rl.close()
+    if (answer === 'y' || answer === 'yes') cb(null, true)
+    else cb(null, false)
+  });
+}
+
 // String, Number -> ({x, y} -> String)
 function tileToUrlMapper (template, zoom) {
   return function (pt) {
     return buildUrl(template, pt.x, pt.y, zoom)
-  })
+  }
 }
 
 // String, Number, Number, Number -> String
@@ -46,25 +71,30 @@ function tilesInRect (bbox) {
 function toRect (lat, lon, radius, zoom) {
   return {
     top: lonToTile(lon - radius, zoom),
-    left: latToTile(lat - radius, zoom),
-    bottom: lonToTile(lon - radius, zoom),
-    right: latToTile(lat + radius, zoom)
+    left: latToTile(lat + radius, zoom),
+    bottom: lonToTile(lon + radius, zoom),
+    right: latToTile(lat - radius, zoom)
   }
 }
 
 // Number -> Number
-function lonToTile (lat, zoom) {
+function lonToTile (lon, zoom) {
   var n = Math.pow(2, zoom)
   var x = n * ((lon + 180) / 360)
-  return x
+  return Math.floor(x)
 }
 
 // Number -> Number
 function latToTile (lat, zoom) {
   var n = Math.pow(2, zoom)
   var latRadians = lat * (Math.PI / 180)
-  var y = n * (1 - (Math.log(Math.tan(latRadians) + Math.sec(latRadians)) / Math.PI)) / 2
-  return y
+  var y = n * (1 - (Math.log(Math.tan(latRadians) + sec(latRadians)) / Math.PI)) / 2
+  return Math.floor(y)
+}
+
+// Number -> Number
+function sec (rad) {
+  return 1 / Math.cos(rad)
 }
 
 // String -> Boolean
