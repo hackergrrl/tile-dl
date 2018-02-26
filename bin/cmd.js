0dc0e92e277470f8624fc4cdd5891c4a21a7c64b
Stephen Whitmore <sww@eight45.net>
2018-02-26 15:53:33 -0800

    dev: hey it works

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,32 +1,91 @@
 #!/usr/bin/env node
 
-var minimistAliases = { 'z': 'zoom', 't': 'template', 'r': 'radius' }
+var minimistAliases = {
+  'z': 'zoom',
+  't': 'template',
+  'r': 'radius',
+  'o': 'output'
+}
 var argv = require('minimist')(process.argv, { alias: minimistAliases })
 var fs = require('fs')
 var path = require('path')
 var readline = require('readline')
+var mkdirp = require('mkdirp')
+var http = require('http')
+var https = require('https')
 
-if (!argv.zoom || !argv.lat || !argv.lon || !argv.radius || !argv.template ||
-  !isTemplateValid(argv.template)) {
+if (!argv.output || !argv.zoom || !argv.lat || !argv.lon || !argv.radius || !argv.template || !isTemplateValid(argv.template)) {
   printUsageAndExit()
+  return
 }
 
+// 1. Compute tiles
 var bbox = toRect(Number(argv.lat), Number(argv.lon), Number(argv.radius), Number(argv.zoom))
 var tiles = tilesInRect(bbox)
 
+// 2. Compute URLs
 var mapFn = tileToUrlMapper(argv.template, Number(argv.zoom))
-var urls = tiles.map(mapFn)
+var urlInfo = tiles.map(mapFn)
 
-var text = 'This will make ' + urls.length + ' HTTP requests. Proceed? [y/N] '
+// 3. Ask user if they really want to do this
+var text = 'This will make ' + urlInfo.length + ' HTTP requests. Proceed? [y/N] '
 prompt(text, function (err, proceed) {
   if (err) throw err
   if (proceed) {
-    console.log('TODO: download')
+    onProceed()
   } else {
     console.log('aborted')
     process.exit(1)
   }
 })
+
+// 4. Make http requests
+function onProceed () {
+  var pending = urlInfo.length
+  urlInfo.forEach(function (data) {
+    // Make output directory
+    var outpath = outputPath(argv.output, data.x, data.y, data.z)
+    mkdirp.sync(path.dirname(outpath))
+
+    // Download file + write
+    console.log('GET', data.x, data.y, data.z, data.url)
+    getAndWriteUrl(data.url, outpath, function (err) {
+      if (err) throw err
+      console.log('GOT', data.x, data.y, data.z)
+      if (!--pending) onDownloaded()
+    })
+  })
+}
+
+function onDownloaded () {
+  console.log('done')
+}
+
+function getAndWriteUrl (url, out, cb) {
+  var web = url.startsWith('https') ? https : http
+  web.get(url, function (res) {
+    if (res.statusCode !== 200) return cb(new Error('status ' + res.statusCode))
+
+    var ws = fs.createWriteStream(out)
+    res.on('data', function (data) {
+      ws.write(data)
+    })
+    res.once('end', function () {
+      ws.end()
+      cb()
+    })
+  })
+}
+
+// String, Number, Number, Number -> String
+function outputPath (output, x, y, z) {
+  var pathPieces = output
+    .replace('{x}', x)
+    .replace('{y}', y)
+    .replace('{z}', z)
+    .split('/')
+  return path.join.apply(path, pathPieces)
+}
 
 function prompt (text, cb) {
   var rl = readline.createInterface({
@@ -44,7 +103,12 @@ function prompt (text, cb) {
 // String, Number -> ({x, y} -> String)
 function tileToUrlMapper (template, zoom) {
   return function (pt) {
-    return buildUrl(template, pt.x, pt.y, zoom)
+    return {
+      url: buildUrl(template, pt.x, pt.y, zoom),
+      x: pt.x,
+      y: pt.y,
+      z: zoom
+    }
   }
 }
 
@@ -70,10 +134,10 @@ function tilesInRect (bbox) {
 // Number, Number, Number -> {left, right, top, bottom}
 function toRect (lat, lon, radius, zoom) {
   return {
-    top: lonToTile(lon - radius, zoom),
-    left: latToTile(lat + radius, zoom),
-    bottom: lonToTile(lon + radius, zoom),
-    right: latToTile(lat - radius, zoom)
+    top: latToTile(lat + radius, zoom),
+    left: lonToTile(lon - radius, zoom),
+    bottom: latToTile(lat - radius, zoom),
+    right: lonToTile(lon + radius, zoom)
   }
 }
 
